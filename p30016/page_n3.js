279e79482943392b9c0a298b4516ba5945383946
weitong <99207432a@gmail.com>
2016-05-09 12:37:42 +0800

    p30016:skip done

--- p30016/page_n3.js
+++
@@ -1,6 +0,0 @@
-$('#QID_ddypceu8 :input').click(function(){
-    if ( $('#QID_ddypceu8 :input:checked').length > 3 ) {
-        alert('限選三個項目');
-        $(this).prop('checked', false);
-    }
-});
--- p30016/page_n7.js
+++
@@ -1,6 +0,0 @@
-$('#QID_j2j54t9m :input').click(function(){
-    if ( $('#QID_j2j54t9m :input:checked').length > 3 ) {
-        alert('限選三個項目');
-        $(this).prop('checked', false);
-    }
-});
