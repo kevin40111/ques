fb6ab4d5634d686834256f6afc8bf4ead81b8cf8
ken <momo60104@hotmail.com>
2016-08-25 17:05:04 +0800

    master: modify footer url and peer page check

--- teacherpeer104/footer.php
+++ teacherpeer104/footer.php
@@ -6,7 +6,7 @@
     <div class="ui mini horizontal bulleted link list">
         <span class="item"><?=gethostname()?></span>
         <span class="item">Copyright © 2015 教育部師資培育及藝術教育司委託，國立臺灣師範大學教育研究與評鑑中心承辦</span>
-        <a class="item" target="_blank" href="/13/report">聯絡我們</a>
+        <a class="item" target="_blank" href="/ques/13/report">聯絡我們</a>
     </div>
 
 </div>
