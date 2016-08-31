4639b44dc76a8d7d4d214842bd998413711608ea
tim <tim72117@gmail.com>
2016-08-31 15:28:42 +0800

    master: change footer style

--- teacherpeer104/footer.php
+++ teacherpeer104/footer.php
@@ -1,12 +1,4 @@
 
-<div class="ui divider"></div>
-
-<div class="ui center aligned basic segment">
-
-    <div class="ui mini horizontal bulleted link list">
-        <span class="item"><?=gethostname()?></span>
-        <span class="item">Copyright © 2015 教育部師資培育及藝術教育司委託，國立臺灣師範大學教育研究與評鑑中心承辦</span>
-        <a class="item" target="_blank" href="/ques/13/report">聯絡我們</a>
-    </div>
-
-</div>
+<span class="item">Copyright © 2015 教育部師資培育及藝術教育司委託，國立臺灣師範大學教育研究與評鑑中心承辦</span>
+<a class="item" target="_blank" href="/ques/13/report">聯絡我們</a>
+<span class="item"><?=gethostname()?></span>
