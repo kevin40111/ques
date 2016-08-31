205335a8103732be3e1b8ff4f7f09a402314c7df
ken <momo60104@hotmail.com>
2016-08-31 17:21:52 +0800

    master: filtering same school value and ques set

---
+++ newteacher104/page_n5.js
@@ -0,0 +1,7 @@
+$('input[name=p5q1c12]').click(function() {
+    if ($(this).is(':checked')) {
+        $('#QID_9nf92265,#QID_yp9ysir1').qhide();
+    } else {
+        $('#QID_9nf92265,#QID_yp9ysir1').qshow();
+    }
+});
--- teacheradmin104/page_n4.js
+++ teacheradmin104/page_n4.js
@@ -1,42 +1,81 @@
 $(function () {
-    var input_school = [];
-
+    var num = 0;
+    var count = 0;
+    var ques = [];
     for (var i = 2; i <= 6; i++) {
+        var inputs = [];
+        var selects = [];
         for (var j = 1; j <= 3; j++) {
-            input_school.push('input[name=p4q'+i+'t'+j+']'); 
+            inputs.push('input[name=p4q'+i+'t'+j+']');
+            selects.push('s'+count);
+            count ++;
         }
+        ques[num] = {};
+        ques[num].inputs = inputs;
+        ques[num].selects = selects;
+        num ++;
     }
 
-    var reload_school = function() {
+    var load_school = function() {
         $.getJSON('public/school', function( data ) {
             var option = '<option value="-9">請選擇</option>';
             for (var i in data) {
                 option += '<option value="'+data[i].id+'">'+data[i].name+'</option>';
             }
             var count = 0;
-            for(var i in input_school) {
-                count++;
-                if (count > 3) {count = 1;}
-                $(input_school[i]).css({"position": "absolute", "z-index": "-100","left":"-999999px"});
-                $(input_school[i]).after('<label>第'+count+'所 : </label><select name="s'+i+'">'+option+'</select>');
-                $('select[name=s'+i+']').change(function() {
-                    var num         = $(this).attr('name').split('s')[1];
-                    var selected    = $(this).find('option:selected');
-                    set_school(input_school[num],selected);
-                });
-            } 
+            var x = 0;
+            for(var i in ques) {
+                for(var j in ques[i].inputs) {
+                    count++;
+                    if (count > 3) {count = 1;}
+                    $(ques[i].inputs[j]).css({"position": "absolute", "z-index": "-100","left":"-999999px"});
+                    $(ques[i].inputs[j]).after('<label>第'+count+'所 : </label><select name="s'+x+'" data="'+ques[i].inputs[j]+'">'+option+'</select>');
+                    $('select[name=s'+x+']').change(function() {
+                        var input      = $(this).attr('data');
+                        var selected   = $(this).find('option:selected');
+                        set_school(input,selected,option);
+                    });
+                    x++;
+                }
+            }
         });
     }
 
-    var set_school = function (to,element) {
-        var school = element.text();
-        if (element.val() == '-9') {
+    var set_school = function (input,selected,option) {
+       ;
+        var school = selected.text();
+        if (selected.val() == '-9') {
             school = '';
         }
-        $(to).val(school);
-        $(to).trigger("change");
+        $(input).val(school);
+        $(input).trigger("change");
+
+        disable_school(input,option);
+    }
+    var disable_school = function (input,option) {
+        var key = 0;
+        for (var i in ques) {
+            for (var j in ques[i].inputs) {
+                if (ques[i].inputs[j] == input) {
+                    key = i;
+                    break;
+                }
+            }
+        }
+        for (var i in ques[key].inputs) {
+            $('select[name="'+ques[key].selects[i]+'"] option').removeAttr('disabled');
+        }
+
+        for (var i in ques[key].inputs) {
+            var elem1 = $('select[name="'+ques[key].selects[i]+'"]');
+            for (var j = 0 ; j < ques[key].inputs.length; j++) {
+                if (ques[key].inputs[i] != ques[key].inputs[j] && elem1.val() != '-9') {
+                    $('select[name="'+ques[key].selects[j]+'"] option[value=' + elem1.val() + ']').prop('disabled',true);
+                }
+            }
+        }
     }
 
-    reload_school();
+    load_school();
 });
 
