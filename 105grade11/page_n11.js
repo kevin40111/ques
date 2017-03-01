c964681b1b05d63587a88bfd464c303aac20c0cb
tim <tim72117@gmail.com>
2017-03-01 12:13:31 +0800

    master: 105grade11 p11q1c3

--- 105grade11/page_n11.js
+++ 105grade11/page_n11.js
@@ -1,11 +1,11 @@
 
-$(':input[name=p11q1c4]').change(function() {
+$(':input[name=p11q1c3,:input[name=p11q1c4]').change(function() {
     if ($(this).is(':checked')) {
         $('#QID_2y4l2moo,#QID_syxxm351,#QID_ddyx9wij,#QID_9bt8itz2').qhide();
     }
 });
 
-$(':input[name=p11q1c1],:input[name=p11q1c2],:input[name=p11q1c3]').change(function() {
+$(':input[name=p11q1c1],:input[name=p11q1c2]]').change(function() {
     if ($(this).is(':checked')) {
         $('#QID_2y4l2moo,#QID_syxxm351,#QID_ddyx9wij,#QID_9bt8itz2').qshow();
     }
