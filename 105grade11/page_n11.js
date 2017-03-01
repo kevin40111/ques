9f039cadb3a1e5bbfd16ecb91d047b2b565cdcf9
tim <tim72117@gmail.com>
2017-03-01 12:25:58 +0800

    105grade11 p11

--- 105grade11/page_n11.js
+++ 105grade11/page_n11.js
@@ -1,12 +1,8 @@
 
-$(':input[name=p11q1c3],:input[name=p11q1c4]').change(function() {
-    if ($(this).is(':checked')) {
-        $('#QID_2y4l2moo,#QID_syxxm351,#QID_ddyx9wij,#QID_9bt8itz2').qhide();
-    }
-});
-
-$(':input[name=p11q1c1],:input[name=p11q1c2]').change(function() {
-    if ($(this).is(':checked')) {
+$(':input[name=p11q1c1],:input[name=p11q1c2],:input[name=p11q1c3],:input[name=p11q1c4]').change(function() {
+    if ($(':input[name=p11q1c1]').is(':checked') || $(':input[name=p11q1c2]').is(':checked')) {
         $('#QID_2y4l2moo,#QID_syxxm351,#QID_ddyx9wij,#QID_9bt8itz2').qshow();
+    } else {
+        $('#QID_2y4l2moo,#QID_syxxm351,#QID_ddyx9wij,#QID_9bt8itz2').qhide();
     }
 });
