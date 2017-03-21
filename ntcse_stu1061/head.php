959f82e6c1067112227570f11450ffd09bfa1017
iamCPS <va2946@gmail.com>
2017-03-21 22:17:51 +0800

    master: ques css head.php modify

--- ntcse_stu1061/head.php
+++ ntcse_stu1061/head.php
@@ -1,5 +1,1 @@
-<div class="onepcssgrid-1000" style="margin-top:0">
-	<div class="onerow">
-		<div class="colfull" style="height:345px;background-size:822px 345px;background-position:center;background-repeat:no-repeat;margin-top:10px;padding:0;color:#fff;background-image: url(/<?=$doc->dir?>/resource/images?f=0.jpg)"></div>
-	</div>
-</div>
+<img class="ui centered image" src="/resource/<?=$doc->dir?>/images/0.jpg">
