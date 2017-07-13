935ee7d84a411d74d0a35d810a5e7ee2baeb137f
iamCPS <va2946@gmail.com>
2017-07-13 12:19:17 +0800

    master: bugfix in NCU p1.p3.p5(email&phone setting)

--- 105ncu_p3/page_n6_check.js
+++ 105ncu_p3/page_n6_check.js
@@ -4,11 +4,11 @@ var patt_cellphone = /^09[\d]{8,10}$/;
 // var patt_home_number = /^[\d#-]{10,20}$/;
 //var patt_home_number = /\(\d{2}\)\d{6,8}/;
 
-if( !patt_email.test($('input[name=p6q4t2]').val()) && (!patt_cellphone.test($('input[name=p6q4t1]').val()) && $('input[name=p6q4t1]').val() != "")) {
+if( (!patt_email.test($('input[name=p6q4t2]').val()) && $('input[name=p6q4t2]').val() != "") && (!patt_cellphone.test($('input[name=p6q4t1]').val()) && $('input[name=p6q4t1]').val() != "")) {
     $('#QID_bt0s26ld').parent().addClass('mark');
     alert('「電子信箱」與「手機」 格式錯誤');
     return false;
-}else if( !patt_email.test($('input[name=p6q4t2]').val()) ){
+}else if( !patt_email.test($('input[name=p6q4t2]').val()) && $('input[name=p6q4t2]').val() != ""){
     $('#QID_bt0s26ld').parent().addClass('mark');
     alert('電子信箱 格式錯誤');
     return false;
